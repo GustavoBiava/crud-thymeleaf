20eb8c035242529e24935a3166cc4a1a57e2ee49
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-31 02:17:55 -0300

    UserService methods

--- src/main/java/com/biava/crudthymeleaf/services/UserService.java
+++ src/main/java/com/biava/crudthymeleaf/services/UserService.java
@@ -1,11 +1,38 @@
 package com.biava.crudthymeleaf.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.biava.crudthymeleaf.entities.User;
 import com.biava.crudthymeleaf.repositories.UserRepository;
 
+@Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
+	
+	public void update(User user) {
+		repository.save(user);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new RuntimeException());
+	}
 }
